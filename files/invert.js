d7ffb7cf71176a1cf0cb666f6a1b5aaed88073ec
joshjennings98 <jenninjl01@gmail.com>
2020-10-14 17:47:47 +0100

    Added check for 'prefers-color-scheme'

--- files/invert.js
+++ files/invert.js
@@ -1,29 +1,32 @@
 "use strict";
 
-var textArr = ["Inverted mode", "Normal mode"];
-var classMethods = ["remove", "add"];
+if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
 
-var nightmodeDiv = document.createElement('div');
-    nightmodeDiv.id = "invmode";
-    nightmodeDiv.innerText = textArr[0];
-  
-  document.body.appendChild(nightmodeDiv);
+  var textArr = ["Inverted mode", "Normal mode"];
+  var classMethods = ["remove", "add"];
 
-var el = document.getElementsByTagName("html")[0];
-var acbox = document.getElementById("invmode"),
-  textNode = acbox.firstChild,
-  toggled = sessionStorage.getItem("inverted") == 'true';
-  var selector = Number(toggled);
-  textNode.data = textArr[selector];
-  el.classList[classMethods[selector]]("inverted");
-  
-acbox.addEventListener(
-  "ontouchstart" in window ? "touchend" : "click",
-  function() {
-    var selector = Number((toggled = !toggled));
+  var nightmodeDiv = document.createElement('div');
+      nightmodeDiv.id = "invmode";
+      nightmodeDiv.innerText = textArr[0];
+    
+    document.body.appendChild(nightmodeDiv);
+
+  var el = document.getElementsByTagName("html")[0];
+  var acbox = document.getElementById("invmode"),
+    textNode = acbox.firstChild,
+    toggled = sessionStorage.getItem("inverted") == 'true';
+    var selector = Number(toggled);
     textNode.data = textArr[selector];
     el.classList[classMethods[selector]]("inverted");
-    sessionStorage.setItem("inverted", toggled);
-  },
-  false
-);
+    
+  acbox.addEventListener(
+    "ontouchstart" in window ? "touchend" : "click",
+    function() {
+      var selector = Number((toggled = !toggled));
+      textNode.data = textArr[selector];
+      el.classList[classMethods[selector]]("inverted");
+      sessionStorage.setItem("inverted", toggled);
+    },
+    false
+  );
+}
