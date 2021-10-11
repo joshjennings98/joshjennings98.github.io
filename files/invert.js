1537819fc76e4231c947fca8bed8d171babde4bf
Josh <jenninjl01@gmail.com>
2021-10-11 23:38:18 +0100

    Add CO2 thing to footer

--- files/invert.js
+++ files/invert.js
@@ -1,45 +1,64 @@
 "use strict";
 
-function localStorageAvailable(){ 
-  var test = 'test';
-  try {
-    localStorage.setItem(test, test);
-    localStorage.removeItem(test);
-    return true;
-  } catch(e) {
-    return false;
-  }
+function localStorageAvailable() {
+    var test = 'test';
+    try {
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+function setFooterLightDarkMode() {
+    if (window.matchMedia('(prefers-color-scheme:dark)').matches) {
+        if (!toggled) {
+            document.getElementById("wcb").classList.add('wcb-d')
+        } else {
+            document.getElementById("wcb").classList.remove('wcb-d')
+        }
+    } else {
+        if (!toggled) {
+            document.getElementById("wcb").classList.remove('wcb-d')
+        } else {
+            document.getElementById("wcb").classList.add('wcb-d')
+        }
+    }
 }
 
 if (window.matchMedia('(prefers-color-scheme)').media !== 'not all' && localStorageAvailable() === true) { // using localStorage because it's not a variable that needs to be secure or anything
 
-  document.body.style.margin="2em auto";
+    document.body.style.margin = "2em auto";
 
-  var textArr = ["Inverted mode", "Normal mode"];
-  var classMethods = ["remove", "add"];
+    var textArr = ["Inverted mode", "Normal mode"];
+    var classMethods = ["remove", "add"];
 
-  var nightmodeDiv = document.createElement('div');
-      nightmodeDiv.id = "invmode";
-      nightmodeDiv.innerText = textArr[0];
-    
+    var nightmodeDiv = document.createElement('div');
+    nightmodeDiv.id = "invmode";
+    nightmodeDiv.innerText = textArr[0];
+
     document.body.appendChild(nightmodeDiv);
 
-  var el = document.getElementsByTagName("html")[0];
-  var acbox = document.getElementById("invmode"),
-    textNode = acbox.firstChild,
-    toggled = localStorage.getItem("inverted") == 'true';
+    var el = document.getElementsByTagName("html")[0];
+    var acbox = document.getElementById("invmode"),
+        textNode = acbox.firstChild,
+        toggled = localStorage.getItem("inverted") == 'true';
     var selector = Number(toggled);
     textNode.data = textArr[selector];
     el.classList[classMethods[selector]]("inverted");
-    
-  acbox.addEventListener(
-    "ontouchstart" in window ? "touchend" : "click",
-    function() {
-      var selector = Number((toggled = !toggled));
-      textNode.data = textArr[selector];
-      el.classList[classMethods[selector]]("inverted");
-      localStorage.setItem("inverted", toggled);
-    },
-    false
-  );
+
+    setFooterLightDarkMode()
+
+    acbox.addEventListener(
+        "ontouchstart" in window ? "touchend" : "click",
+        function() {
+            var selector = Number((toggled = !toggled));
+            textNode.data = textArr[selector];
+            el.classList[classMethods[selector]]("inverted");
+            localStorage.setItem("inverted", toggled);
+            setFooterLightDarkMode()
+        },
+        false
+    );
 }
